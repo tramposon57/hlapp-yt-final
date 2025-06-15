c6c0ab6800e12f036f31aade33450520a203a741
tramposon57 <tramposon57@gmail.com>
2025-06-15 15:04:40 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-547MC72L');`,
+})(window,document,'script','dataLayer','GTM-P7XJCNF5');`,
           }}
         />
         {/* End Google Tag Manager */}
@@ -35,7 +35,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-547MC72L"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P7XJCNF5"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
